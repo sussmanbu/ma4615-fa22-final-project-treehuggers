2adc56378220be9a930876ada9a394b894937d5d
Jenna Riedl <jriedl@bu.edu>
2022-12-18 18:44:10 -0500

    load and clean, new dataset for race and big picture edits

--- static/load_and_clean_data.R
+++ static/load_and_clean_data.R
@@ -27,17 +27,13 @@ CAcountytract_clean <- CAcountytract %>% rename("remaining" = "TYPE; FULLCODE; S
 ### CLEAN ASTHMA DATA ###
 asthma <- read_csv(here::here("dataset", "asthma.csv"), show_col_types=FALSE)
 
-# The cleaned "kids" data features asthma data on people who are from ages 0-17.
+# The cleaned "kids" dataset: people who are from ages 0-17.
 asthmaCA_kids <- asthma %>% filter(AGE_GROUP == "0-17 years") %>%
   filter(!is.na(AGE_ADJUSTED_HOSPITALIZATION_RATE)) %>%
   filter(!is.na(NUMBER_OF_HOSPITALIZATIONS)) %>%
   select(-COMMENT)
 
-# The below section looks at the racial and ethnic backgrounds of asthma sufferers in California. 
-# Since air pollution is often a bigger issue for those who experience 
-# environmental injustice, this could help shed light on where that is happening. 
-# If some of the counties like up with those with high rates of childhood asthma, 
-# it becomes even more convincing.
+# The cleaned "race/ethnicity" dataset:
 asthmaCA_race_ethnicity <- asthma %>% filter(STRATA == "Race/ethnicity") %>%
   filter(!is.na(NUMBER_OF_HOSPITALIZATIONS)) %>%
   filter(!is.na(AGE_ADJUSTED_HOSPITALIZATION_RATE)) %>%
@@ -77,3 +73,86 @@ joined3 <- clean_parks_data %>% inner_join(asthmaCA_kids_v_adults, by = "county_
 colnames(asthmaCA_race_ethnicity) <- c("county_name", "year", "strata", "race_ethnicity", "age_group", "number_hospitalizations", "age_adjusted_hospitalization_rate")
 asthmaCA_race_ethnicity <- asthmaCA_race_ethnicity %>% mutate(county_name = str_to_upper(county_name))
 joined4 <- clean_parks_data %>% inner_join(asthmaCA_race_ethnicity, by = "county_name")
+
+
+### CLEANING RACE/ETHNICITY AND PARKS DATA ###
+
+# getting the parks data for each individual census tract
+
+parks_data_by_tract <- parks_asthmaCA_race_ethnicity %>%
+  group_by(tractcode) %>%
+  summarize(open_parks_tract = mean(open_parks_tract), tract_area_sqmiles = mean(tract_area_sqmiles), total_open_park_area_sqmiles = mean(total_open_park_area_sqmiles), county_name = county_name)
+
+# summing to get the total areas for parks and general land for each county
+
+parks_data_by_county <- parks_data_by_tract %>%
+  group_by(county_name) %>%
+  summarize(total_county_area_sqm = sum(tract_area_sqmiles), total_county_park_area_sqm = sum(total_open_park_area_sqmiles), county_num_parks = sum(open_parks_tract))
+
+# creating individual datasets for each race strata level and total/POC
+
+parks_asthmaCA_race_2018 <- parks_asthmaCA_race_ethnicity %>%
+  filter(year == "2018")
+
+hospitalizations_white <- parks_asthmaCA_race_2018 %>%
+  filter(race_ethnicity == "White") %>%
+  group_by(county_name) %>%
+  summarize(number_hospitalizations_white = mean(number_hospitalizations))
+
+hospitalizations_black <- parks_asthmaCA_race_2018 %>%
+  filter(race_ethnicity == "Black") %>%
+  group_by(county_name) %>%
+  summarize(number_hospitalizations_black = mean(number_hospitalizations))
+
+hospitalizations_hispanic <- parks_asthmaCA_race_2018 %>%
+  filter(race_ethnicity == "Hispanic") %>%
+  group_by(county_name) %>%
+  summarize(number_hospitalizations_hispanic = mean(number_hospitalizations))
+
+hospitalizations_asian_pi <- parks_asthmaCA_race_2018 %>%
+  filter(race_ethnicity == "Asian/PI") %>%
+  group_by(county_name) %>%
+  summarize(number_hospitalizations_asian_pi = mean(number_hospitalizations))
+
+hospitalizations_ai_an <- parks_asthmaCA_race_2018 %>%
+  filter(race_ethnicity == "AI/AN") %>%
+  group_by(county_name) %>%
+  summarize(number_hospitalizations_ai_an = mean(number_hospitalizations))
+
+hospitalizations_poc <- asthmaCA_race_ethnicity %>%
+  filter(!race_ethnicity == "White") %>%
+  filter(year == "2018") %>%
+  group_by(county_name) %>%
+  summarize(number_hospitalizations_poc = sum(number_hospitalizations))
+
+hospitalizations_total <- asthmaCA_race_ethnicity %>%
+  filter(year == "2018") %>%
+  group_by(county_name) %>%
+  summarize(number_hospitalizations_total = sum(number_hospitalizations))
+
+# joining datasets together via shared variable "county_name"
+CA_county_asthma_parks <-
+  full_join(parks_data_by_county, hospitalizations_white, by = c("county_name" = "county_name"))
+
+CA_county_asthma_parks <- CA_county_asthma_parks %>%
+  full_join(hospitalizations_poc, by = c("county_name" = "county_name"))
+
+CA_county_asthma_parks <- CA_county_asthma_parks %>%
+  full_join(hospitalizations_hispanic, by = c("county_name" = "county_name"))
+
+CA_county_asthma_parks <- CA_county_asthma_parks %>%
+  full_join(hospitalizations_asian_pi, by = c("county_name" = "county_name"))
+
+# As the AI/AN category values were all 0 or NA, we decided it was not useful to keep analyzing that information. 
+# This is an extremely important missing segment of data, and the reason this information is not available should be further explored.
+
+CA_county_asthma_parks <- CA_county_asthma_parks %>%
+  full_join(hospitalizations_black, by = c("county_name" = "county_name"))
+
+CA_county_asthma_parks <- CA_county_asthma_parks %>%
+  full_join(hospitalizations_total, by = c("county_name" = "county_name"))
+
+CA_county_asthma_parks <- CA_county_asthma_parks %>%
+  filter(!is.na(total_county_area_sqm))
+
+save(CA_county_asthma_parks, file = here::here("dataset/CA_county_asthma_parks.RData"))
