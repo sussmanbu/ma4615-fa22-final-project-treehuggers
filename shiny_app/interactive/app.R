41aa366e8f6d2113d04beecf80c97911195639bd
Maecey Niksch <mjniksch@bu.edu>
2022-12-18 18:58:00 -0500

    shiny app updates

--- shiny_app/interactive/app.R
+++ shiny_app/interactive/app.R
@@ -6,15 +6,18 @@
 #
 #    http://shiny.rstudio.com/
 #
+
+# Load Packages
 library(tidyverse)
 library(sf)
 library(USAboundaries)
 library(tmap)
 library(shiny)
 
-map_kids_v_adults <- load(here::here("shiny_app/interactive/map_kids_v_adults.RData"))
-map_race_ethnicity <- load(here::here("shiny_app/interactive/map_race_ethnicity.RData"))
+# Load Data
+df <- load(here::here("shiny_app/interactive/map_race_ethnicity.RData"))
 
+# Load Various Mapping Requirements
 epsg_CA <- 26943
 epsg_wgs84 <- 4326
 CA_state <- USAboundaries::us_states() %>% filter(name == "California") %>%
@@ -24,64 +27,48 @@ CA_counties <- USAboundaries::us_counties(states = "California") %>%
   select(-namelsad, -state_name, -state_abbr, -jurisdiction_type) %>% 
   mutate("county_name" = str_to_upper(name)) %>% select(-name)
 
-strata_choices <- map_kids_v_adults %>% 
-  pull(strata_name) %>% unique() %>% sort()
-race_choices <- map_race_ethnicity %>%
-  pull(race_ethnicity) %>% unique() %>% sort()
+# Create Variable Choices for Reactive Filter
+race_choices <- c("Black", "Hispanic", "White")
+
+county_choices <- df %>% pull(county_name) %>% unique() %>% sort()
+
 
 ui <- fluidPage(
-    
+  
+  # Application title
   titlePanel("Asthma Hospitalizations by County"),
-    selectInput(inputId = "strata", 
-                label = "County Hospitalizations by Age Group (Kid vs. Adult)", 
-                choices = strata_choices),
-        mainPanel(
-           plotOutput("map")
-           ),
-    selectInput(inputId = "race",
-                label = "County Hospitalizations by Race/Ethnicity",
-                choices = race_choices),
-        mainPanel(
-            plotOutput("map2"),
-            plotOutput("map3")
-        )
-    )
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "race",
+                  label = "Race/Ethnicity",
+                  choices = race_choices
+      )),
+  mainPanel(
+    plotOutput("map")
+  ))
+)
 
 
 server <- function(input, output) {
-
-    output$map <- renderPlot({
-      
-        map_kids_v_adults %>% 
-         filter(strata_name == input$strata) %>% 
-          group_by(county_name) %>% 
-          mutate("strata_hospitalizations" = sum(number_hospitalizations)) %>% 
-        tm_shape() + tm_polygons(col = "strata_hospitalizations", n = 4, palette = "viridis") + 
-        tm_shape(CA_counties) + tm_borders() + 
-        tm_shape(CA_state) + tm_borders(lwd = 2)
-    
-    })
-     output$map2 <- renderPlot({
-      
-      map_race_ethnicity %>% 
-        filter(race_ethnicity == input$race) %>% 
-        group_by(county_name) %>% 
-        mutate("race_hospitalizations" = sum(number_hospitalizations)) %>% 
-        tm_shape() + tm_polygons(col = "race_hospitalizations", n = 4, palette = "viridis", colorNA = NULL) + 
-        tm_shape(CA_counties) + tm_borders() + 
-        tm_shape(CA_state) + tm_borders(lwd = 2)
-      
-    })
-     output$map3 <- renderPlot({
-       map_race_ethnicity %>%
-         filter(race_ethnicity == input$race) %>%
-         group_by(county_name) %>%
-         mutate("race_avg_hospitalization_rate" = mean(age_adjusted_hospitalization_rate)) %>%
-         tm_shape() + tm_polygons(col = "race_avg_hospitalization_rate", palette = "viridis") +
-         tm_shape(CA_counties) + tm_borders() +
-         tm_shape(CA_state) + tm_borders(lwd = 2)
-     })
+  
+  output$map <- renderPlot({
+    df %>%
+      filter(race_ethnicity == input$race) %>%
+      group_by(county_name) %>%
+      tm_shape() + tm_polygons(col = "age_adjusted_hospitalization_rate", palette = "viridis") +
+      tm_shape(CA_counties) + tm_borders() +
+      tm_shape(CA_state) + tm_borders(lwd = 2)
+  })
+  output$barPlot <- renderPlot({
+    df %>% 
+      filter(county_name == input$county) %>% 
+      ggplot(aes(y = race_ethnicity, x = age_adjusted_hospitalization_rate, fill = race_ethnicity)) + 
+      geom_col(position = "dodge") + 
+      labs(title = "Age-Adjusted Hospitalization Rate by Race/Ethnicity in Selected County",
+           x = "Age-Adjusted Hospitalization Rate",
+           y = "Race/Ethnicity", fill = "Race/Ethnicity")
+  })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
