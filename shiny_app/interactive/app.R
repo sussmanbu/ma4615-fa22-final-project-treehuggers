f2c77d4849ba4d34b99e4bb9714b949ae17eb588
Maecey Niksch <mjniksch@bu.edu>
2022-12-19 08:47:48 -0500

    shiny app THAT WORKS

--- shiny_app/interactive/app.R
+++ shiny_app/interactive/app.R
@@ -15,7 +15,9 @@ library(tmap)
 library(shiny)
 
 # Load Data
-df <- load(here::here("shiny_app/interactive/map_race_ethnicity.RData"))
+load(here::here("shiny_app/interactive/map_race_ethnicity.RData"))
+df <- map_race_ethnicity
+class(df)
 
 # Load Various Mapping Requirements
 epsg_CA <- 26943
@@ -26,6 +28,7 @@ CA_counties <- USAboundaries::us_counties(states = "California") %>%
   st_set_crs(epsg_wgs84) %>% st_transform(epsg_CA) %>% 
   select(-namelsad, -state_name, -state_abbr, -jurisdiction_type) %>% 
   mutate("county_name" = str_to_upper(name)) %>% select(-name)
+
 
 # Create Variable Choices for Reactive Filter
 race_choices <- c("Black", "Hispanic", "White")
@@ -43,28 +46,30 @@ ui <- fluidPage(
       selectInput(inputId = "race",
                   label = "Race/Ethnicity",
                   choices = race_choices
-      )),
+      ),
+      selectInput(inputId = "county",
+                  label = "County",
+                  choices = county_choices)
+      ),
   mainPanel(
-    plotOutput("map")
+    plotOutput("map"),
+    plotOutput("barPlot")
   ))
 )
 
-
 server <- function(input, output) {
-  
   output$map <- renderPlot({
-    df %>%
-      filter(race_ethnicity == input$race) %>%
-      group_by(county_name) %>%
+   st_sf(df %>% filter(race_ethnicity == input$race) %>%
+      group_by(county_name)) %>% 
       tm_shape() + tm_polygons(col = "age_adjusted_hospitalization_rate", palette = "viridis") +
       tm_shape(CA_counties) + tm_borders() +
       tm_shape(CA_state) + tm_borders(lwd = 2)
   })
   output$barPlot <- renderPlot({
-    df %>% 
-      filter(county_name == input$county) %>% 
-      ggplot(aes(y = race_ethnicity, x = age_adjusted_hospitalization_rate, fill = race_ethnicity)) + 
-      geom_col(position = "dodge") + 
+    df %>%
+      filter(county_name == input$county) %>%
+      ggplot(aes(y = race_ethnicity, x = age_adjusted_hospitalization_rate, fill = race_ethnicity)) +
+      geom_col(position = "dodge") +
       labs(title = "Age-Adjusted Hospitalization Rate by Race/Ethnicity in Selected County",
            x = "Age-Adjusted Hospitalization Rate",
            y = "Race/Ethnicity", fill = "Race/Ethnicity")
